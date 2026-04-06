8517205105a7b2b8ae889962654325471241b8fb
Sugyk <active.sugyk@gmail.com>
2026-04-06 15:51:01 +0300

    refactor: repository remove whole layer executor for per-function executor

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,13 +1,8 @@
 package repository
 
-type Executor interface{}
-
 type Repository struct {
-	executor Executor
 }
 
-func NewRepository(executor Executor) *Repository {
-	return &Repository{
-		executor: executor,
-	}
+func NewRepository() *Repository {
+	return &Repository{}
 }
