9986d7f82757f5e39e485820ba14d776241411a5
Sugyk <active.sugyk@gmail.com>
2026-04-26 16:09:50 +0300

    fix: ensure correct marshalling body into json before start sending response

--- internal/api/http/handlers/helpers.go
+++ internal/api/http/handlers/helpers.go
@@ -7,9 +7,12 @@ import (
 )
 
 func (h *Handler) sendJSON(ctx context.Context, w http.ResponseWriter, httpStatus int, body any) {
+	data, err := json.Marshal(body)
+	if err != nil {
+		h.handleError(ctx, w, err)
+		return
+	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(httpStatus)
-	if err := json.NewEncoder(w).Encode(body); err != nil {
-		h.logger.Error(ctx, "error send JSON", "error", err)
-	}
+	w.Write(data)
 }
